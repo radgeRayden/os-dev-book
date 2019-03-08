57647aad35e8b0c9786c35dd24dfeb61462f61d2
Westerbly Snaydley <westerbly@gmail.com>
2019-03-08 16:30:47 -0500

    my version of printing a null terminated string

--- boot_sect.asm
+++ boot_sect.asm
@@ -3,28 +3,17 @@
 
     mov ah, 0x0e ; scrolling teletype BIOS routine
     
-    ;first attempt
-    mov al, the_secret
-    int 0x10    ;does this print an X?
-
-    ;second attempt
-    mov al, [the_secret]
-    int 0x10    ;does this print an X?
-
-    ;third attempt
-    mov bx, the_secret
-    add bx, 0x7c00
-    mov al, [bx]
-    int 0x10    ;well does it?
-
-    ;fourth attempt
-    mov al, [0x7c1d]
-    int 0x10    ;what about this one?
-
+    mov bx, 0
+print_loop:
+    mov al, [boot_msg + bx]
+    add bx, 1
+    int 0x10
+    cmp al, 0
+    jnz print_loop
     jmp $ ;$ = current address, so jump forever
 
-the_secret:
-    db "X"    
+boot_msg:
+    db "Booting OS", 0
 
     ;padding and magic BIOS number
 
