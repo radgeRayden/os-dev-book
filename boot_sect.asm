b062c9a2be5354d8e8701ef21053df16c1791c5a
Westerbly Snaydley <westerbly@gmail.com>
2019-03-08 16:16:49 -0500

    added org directive for address offset

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,13 +1,24 @@
 ; vim: set ft=nasm:
+    [org 0x7c00]
 
     mov ah, 0x0e ; scrolling teletype BIOS routine
     
+    ;first attempt
+    mov al, the_secret
+    int 0x10    ;does this print an X?
+
+    ;second attempt
+    mov al, [the_secret]
+    int 0x10    ;does this print an X?
+
+    ;third attempt
     mov bx, the_secret
     add bx, 0x7c00
     mov al, [bx]
     int 0x10    ;well does it?
 
-    mov al, [0x7c14]
+    ;fourth attempt
+    mov al, [0x7c1d]
     int 0x10    ;what about this one?
 
     jmp $ ;$ = current address, so jump forever
