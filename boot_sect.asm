00281353adb793f0c3bdd1d1d0f87dc386001711
Westerbly Snaydley <westerbly@gmail.com>
2019-03-08 15:27:23 -0500

    hello world

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,6 +1,22 @@
-loop:
-    jmp loop
+; vim: set ft=nasm:
 
-times 510-($-$$) db 0
+    mov ah, 0x0e ; scrolling teletype BIOS routine
 
-dw 0xaa55
+    mov al, 'H'
+    int 0x10
+    mov al, 'e'
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+    mov al, '!'
+    int 0x10
+
+    jmp $ ;$ = current address, so jump forever
+
+    times 510-($-$$) db 0 ;pad the boot sector out with zeros
+
+    dw 0xaa55 ; 2-byte magic number for BIOS - identifies boot sector
