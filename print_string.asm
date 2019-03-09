013ae31f22a725606ea38a4ad6243b552e1e59ac
Westerbly Snaydley <westerbly@gmail.com>
2019-03-08 23:34:37 -0500

    modified print_string to avoid polluting the global namespace

--- print_string.asm
+++ print_string.asm
@@ -3,13 +3,13 @@
 print_string:
     pusha
     mov ah, 0x0e
-putchar:
+.putchar:
     mov al, [bx]    ; since we can't address a register in the first operand of cmp, it's
     cmp al, 0       ; necessary to first move the value of [bx] into ax
-    je str_end
+    je  .str_end
     int 0x10
     add bx, 1       ;iterate in place
-    jmp putchar
-str_end:
+    jmp .putchar
+.str_end:
     popa
     ret
